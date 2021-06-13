2fc1aad1b21daf5568d4fd5166bc414b6e0ffd5a
Troy <osuisreallyfun@gmail.com>
2021-06-13 06:30:46 -0400

    .

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const add = (num1, num2) => {
 
-    return +num1 + +num2;
+    if(isNaN(num1) || isNaN(num1)) return console.error(`Error: Result of Add(${num1} + ${num2}) is not a number`);
+
+    return num1 + num2;
 
 }
 
