c80e910fedad11ee75309417fd44c76af7b7f32a
Troy <osuisreallyfun@gmail.com>
2021-06-13 06:44:53 -0400

    .

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const add = (num1, num2) => {
 
-    if(isNaN(num1) || isNaN(num1) || typeof num1 + num2 == 'string') return console.error(`Error: Result of Add(${num1} + ${num2}) is not a number`);
+    if(!num1 || !num2) {
+
+        return console.error('Error: No numbers specified');
+
+    } else if(isNaN(num1) || isNaN(num1) || typeof num1 + num2 == 'string') {
+        
+        return console.error(`Error: Result of Add(${num1} + ${num2}) is not a number`);
+
+    } else {
 
     return parseInt(num1) + parseInt(num2);
 
-}
+    }
 
-console.log(parseInt(3) + parseInt('3'));
+}
 
 module.exports.add = add;
